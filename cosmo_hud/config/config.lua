7d8bdfef6766659ad28225c11c6209a342b52cbf
Xxpromw3mtxX <fra.metta@hotmail.it>
2021-06-30 11:16:15 +0200

    choose if id should be on or off

--- cosmo_hud/config/config.lua
+++ cosmo_hud/config/config.lua
@@ -3,3 +3,4 @@ Config['TickTime'] = 1000
 Config['ShowRadar'] = false -- set to true if you always want the radar to show
 Config['ShowStress'] = false -- set to true if you want a stress indicator
 Config['ShowSpeed'] = true -- set to true if you want speedometer enabled
+Config['ShowServerID'] = true -- set to true if you want the player id enabled
