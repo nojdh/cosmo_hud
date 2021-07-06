455fe1672adc2a8771efa630828b00eddcf91e81
Xxpromw3mtxX <fra.metta@hotmail.it>
2021-07-06 10:51:45 +0200

    added more config

--- cosmo_hud/config/config.lua
+++ cosmo_hud/config/config.lua
@@ -4,3 +4,5 @@ Config['ShowRadar'] = false -- set to true if you always want the radar to show
 Config['ShowStress'] = false -- set to true if you want a stress indicator
 Config['ShowSpeed'] = true -- set to true if you want speedometer enabled
 Config['ShowServerID'] = true -- set to true if you want the player id enabled
+Config['ShowHunger'] = false -- set to true to show hunger all the time
+Config['ShowThirst'] = false -- set to true to show thirst all the time
