8a1d71024bd9ac32511849f8ad6d13c9a3a4e349
Francesco Metta <fra.metta@hotmail.it>
2021-08-05 14:39:08 +0200

    legacy discontinued & removed
    If you need the legacy version, which is an unoptimized and older version, you can still download it from the release page.

--- legacy/cosmo_hud/config/config.lua
+++
@@ -1,5 +0,0 @@
-Config = {}
-Config['TickTime'] = 1000
-Config['ShowRadar'] = false -- set to true if you always want the radar to show
-Config['ShowStress'] = false -- set to true if you want a stress indicator
-Config['ShowSpeed'] = true -- set to true if you want speedometer enabled
