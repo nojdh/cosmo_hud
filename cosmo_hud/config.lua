726ad2a476fd937b01002a403f2a4c3ad171ed43
Francesco Metta <fra.metta@hotmail.it>
2021-08-19 23:19:12 +0200

    new config structure

---
+++ cosmo_hud/config.lua
@@ -0,0 +1,8 @@
+Config = {}
+Config.TickTime = 1000
+Config.ShowRadar = false -- set to true if you always want the radar to show
+Config.ShowStress = false -- set to true if you want a stress indicator
+Config.ShowSpeedO = true -- set to true if you want speedometer enabled
+Config.ShowServerID = true -- set to true if you want the player id enabled
+Config.ShowFuel = false -- set this to true if you want to show fuel (you need LegacyFuel)
+Config.ShowStamina = false -- set this to true to show stamina when sprinting
