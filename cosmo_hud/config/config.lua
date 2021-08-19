5878053d96aac4d3f2dac83ca899efd1879cd03e
Francesco Metta <fra.metta@hotmail.it>
2021-08-19 23:18:33 +0200

    removed old files

--- cosmo_hud/config/config.lua
+++
@@ -1,8 +0,0 @@
-Config = {}
-Config['TickTime'] = 1000
-Config['ShowRadar'] = false -- set to true if you always want the radar to show
-Config['ShowStress'] = false -- set to true if you want a stress indicator (needs to be configurated inside esx_basicneeds)
-Config['ShowSpeed'] = true -- set to true if you want speedometer enabled
-Config['ShowServerID'] = true -- set to true if you want the player id enabled
-Config['ShowFuel'] = false -- set this to true if you want to show fuel (you need LegacyFuel)
-Config['ShowStamina'] = false -- set this to true to show stamina when sprinting
