f33434d9cc21d44daaf0bda584741310e2cfcd29
Francesco Metta <fra.metta@hotmail.it>
2021-09-07 18:19:07 +0200

    Added an event that trigger basicneeds and adds stress

--- cosmo_hud/server/sv_script.lua
+++ cosmo_hud/server/sv_script.lua
@@ -3,3 +3,14 @@ ESX = nil
 TriggerEvent('esx:getSharedObject', function(obj) 
     ESX = obj 
 end)
+
+RegisterServerEvent('cosmo_hud:gainStress')
+AddEventHandler('cosmo_hud:gainStress', function(StressAmount)
+    local xPlayer = ESX.GetPlayerFromId(source)
+	
+	if xPlayer ~= nil then
+        TriggerClientEvent('esx_status:add', source, 'stress', StressAmount)
+	    
+        TriggerClientEvent("cosmo_hud:UpdateStress", source, StressAmount)
+	end
+end)
