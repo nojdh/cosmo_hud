0a48a96e40b767ab7572ffba4c35728f1f3f56bc
Francesco Metta <fra.metta@hotmail.it>
2021-09-23 11:31:34 +0200

    Added function for version checks
    This is still in testing! DO NOT UPDATE!!!!

--- cosmo_hud/server/sv_script.lua
+++ cosmo_hud/server/sv_script.lua
@@ -4,6 +4,29 @@ TriggerEvent('esx:getSharedObject', function(obj)
     ESX = obj 
 end)
 
+-- Version check function
+Citizen.CreateThread(function()
+    updatePath = "/xxpromw3mtxx/cosmo_hud"
+    resourceName = "cosmo_hud ("..GetCurrentResourceName()..")"
+    
+    function checkVersion(err,responseText, headers)
+        curVersion = Config.Version
+    
+        if curVersion ~= responseText and tonumber(curVersion) < tonumber(responseText) then
+            print("\n###############################")
+            print("\n"..resourceName.." is outdated, should be:\n"..responseText.."is:\n"..curVersion.."\nplease update it from https://github.com"..updatePath.."/releases")
+            print("\n###############################")
+        elseif tonumber(curVersion) > tonumber(responseText) then
+            print("You somehow skipped a few versions of "..resourceName.." or the git went offline, if it's still online i advise you to update ( or downgrade? )")
+        else
+            print("^2"..resourceName.." is up to date!^0")
+        end
+    end
+    
+    PerformHttpRequest("https://raw.githubusercontent.com"..updatePath.."/main/version", checkVersion, "GET")
+end)
+
+-- Stress related option
 if Config.ShowStress then
     RegisterServerEvent('cosmo_hud:gainStress')
     AddEventHandler('cosmo_hud:gainStress', function(StressAmount)
