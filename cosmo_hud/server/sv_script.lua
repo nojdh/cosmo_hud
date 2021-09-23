73621c61703d8e47993178b24cbd47aecb440fd1
Francesco Metta <fra.metta@hotmail.it>
2021-09-23 11:32:53 +0200

    modified function
    This is still in testing! DO NOT UPDATE!!!!

--- cosmo_hud/server/sv_script.lua
+++ cosmo_hud/server/sv_script.lua
@@ -9,14 +9,12 @@ Citizen.CreateThread(function()
     updatePath = "/xxpromw3mtxx/cosmo_hud"
     resourceName = "cosmo_hud ("..GetCurrentResourceName()..")"
     
-    function checkVersion(err,responseText, headers)
-        curVersion = Config.Version
-    
-        if curVersion ~= responseText and tonumber(curVersion) < tonumber(responseText) then
+    function checkVersion(err,responseText, headers)    
+        if Config.Version ~= responseText and tonumber(Config.Version) < tonumber(responseText) then
             print("\n###############################")
-            print("\n"..resourceName.." is outdated, should be:\n"..responseText.."is:\n"..curVersion.."\nplease update it from https://github.com"..updatePath.."/releases")
+            print("\n"..resourceName.." is outdated, should be:\n"..responseText.."is:\n"..Config.Version.."\nplease update it from https://github.com"..updatePath.."/releases")
             print("\n###############################")
-        elseif tonumber(curVersion) > tonumber(responseText) then
+        elseif tonumber(Config.Version) > tonumber(responseText) then
             print("You somehow skipped a few versions of "..resourceName.." or the git went offline, if it's still online i advise you to update ( or downgrade? )")
         else
             print("^2"..resourceName.." is up to date!^0")
