327e535fb375b8e2ccf44c719ab435a5a65b1c9f
Xxpromw3mtxX <32299416+Xxpromw3mtxX@users.noreply.github.com>
2022-02-16 21:25:52 +0100

    Update sv_script.lua

--- cosmo_hud/server/sv_script.lua
+++ cosmo_hud/server/sv_script.lua
@@ -5,25 +5,27 @@ TriggerEvent('esx:getSharedObject', function(obj)
 end)
 
 -- Version Checker
-PerformHttpRequest("https://raw.githubusercontent.com/xxpromw3mtxx/cosmo_hud/main/.version", function(err, text, headers)
-    Citizen.Wait(2000)
-    local curVer = GetResourceMetadata(GetCurrentResourceName(), "version")
+if Config.CheckVersion then
+    PerformHttpRequest("https://raw.githubusercontent.com/xxpromw3mtxx/cosmo_hud/main/.version", function(err, text, headers)
+        Citizen.Wait(2000)
+        local curVer = GetResourceMetadata(GetCurrentResourceName(), "version")
 
-    if (text ~= nil) then
-        if (text ~= curVer) then
-            print '^1-----------------------------------------^0'
-            print '^1        UPDATE AVAILABLE COSMO_HUD       ^0'
-            print '^1          GET IT ON GITHUB NOW           ^0'
-            print '^1-----------------------------------------^0'
+        if (text ~= nil) then
+            if (text ~= curVer) then
+                print '^1-----------------------------------------^0'
+                print '^1        UPDATE AVAILABLE COSMO_HUD       ^0'
+                print '^1          GET IT ON GITHUB NOW           ^0'
+                print '^1-----------------------------------------^0'
+            else
+                print("^2COSMO_HUD is up to date!^0")
+            end
         else
-            print("^2COSMO_HUD is up to date!^0")
-        end
-    else
-        print '^1----------------------------------------^0'
-        print '^1      ERROR GETTING ONLINE VERSION      ^0'
-        print '^1----------------------------------------^0'
-    end 
-end)
+            print '^1----------------------------------------^0'
+            print '^1      ERROR GETTING ONLINE VERSION      ^0'
+            print '^1----------------------------------------^0'
+        end 
+    end)
+end
 
 -- Stress related option
 if Config.ShowStress then
