93b6fb7d80bc9a8dba5e2817e6400cf97100e876
Xxpromw3mtxX <32299416+Xxpromw3mtxX@users.noreply.github.com>
2022-02-16 21:27:07 +0100

    removed speedo module

--- cosmo_hud/fxmanifest.lua
+++ cosmo_hud/fxmanifest.lua
@@ -4,7 +4,7 @@ game 'gta5'
 name 'cosmo_hud'
 description 'Clean & Simple hud for FiveM inspired by NoPixel 3.0'
 author 'lilfraae'
-version "1.1.11"
+version "1.2.0"
 
 ui_page 'nui/ui.html'
 
@@ -22,7 +22,6 @@ client_scripts {
     'client/cl_script.lua',
     'client/addons/seatbelt.lua',
     'client/addons/stress.lua',
-    'client/addons/speedo.lua',
 }
 
 server_scripts {
