591d7b050e8218152a59fe4bbd40cc8786085b0f
Xxpromw3mtxX <fra.metta@hotmail.it>
2022-02-24 12:35:06 +0100

    refactor(addons/stress): changed variables

--- cosmo_hud/client/addons/stress.lua
+++ cosmo_hud/client/addons/stress.lua
@@ -1,7 +1,7 @@
 Citizen.CreateThread(function()
     while true do
-        Citizen.Wait(Config.TickTime)
-        if Config.ShowStress then
+        Citizen.Wait(Config.ticktime)
+        if Config.showstress then
             local pedID = PlayerPedId()
             -- If ped vehicle speed is higher than 200 km/h it adds a random stress amount between 500 and 2500
             if IsPedInAnyVehicle(pedID, true) then
