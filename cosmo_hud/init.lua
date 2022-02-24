247a57304dd94d5aee61b40d3824e87bca717f85
Xxpromw3mtxX <fra.metta@hotmail.it>
2022-02-24 16:14:41 +0100

    feat(init): new config file

---
+++ cosmo_hud/init.lua
@@ -0,0 +1,26 @@
+shared = {
+    resource = GetCurrentResourceName(),
+    ticktime = 1000,
+    versioncheck = true,
+    showradar = false,
+    gprendering = true,
+    showhealth = false,
+    showstress = true,
+    minstresseffect = 50,
+    showspeedo = true,
+    showfuel = false,
+    showstamina = true,
+    serverid = true,
+
+    --Additional Configs
+
+    -- Seatbelt
+    seatbelt = true,
+    sounds = true,
+    volume = 0.8,
+    beltcontrol = 305,
+    speed = 100.0,
+
+    -- NPWD
+    npwd = true
+}
